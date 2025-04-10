7a6b5f29a2a1fcf9d585ccc065e950fdd9759bf9
Miilanii <milanisobopha5@gmail.com>
2025-04-10 21:51:36 +0200

    Finished with data_types and operators file

---
+++ data_types_and_operators.js
@@ -0,0 +1,76 @@
+
+// Exercise 1: Numbers
+
+
+let price = 45
+let change = 4.99
+
+let addition = price + change
+console.log("Addition",addition)
+
+let subtraction = price - change 
+console.log("Subtraction",subtraction);
+
+let multiplication = price * change 
+console.log("Multiplication", multiplication);
+
+let division = price / change 
+console.log("Division", division);
+
+let modulus = price % change 
+console.log("Modulus", modulus);
+
+let exponentiation = price ** change 
+console.log("Exponentiation",exponentiation);
+
+
+// Exercise 2: Boolean and Operators
+
+// Comparison Operators
+let x = 8;
+let y = 12;
+
+let greaterThan = (x > y);
+console.log("Is x greater than y?", greaterThan); 
+
+let lessThanOrEqualTo = (x <= y);
+console.log("Is x less than or equal to y?", lessThanOrEqualTo);
+
+let equalTo = (x === y);
+console.log("Is x equal to y?", equalTo); 
+
+let notEqualTo = (x !== y);
+console.log("Is x not equal to y?", notEqualTo);
+
+// Logical Operators
+let a = true;
+let b = false;
+
+let andOperator = (a && b);
+console.log("a AND b", andOperator); 
+
+let orOperator = (a || b);
+console.log("a OR b", orOperator);
+
+let notOperator = (!a);
+console.log("NOT a", notOperator); 
+
+// Assignment operators
+
+let p = 10;
+
+p += 5; 
+console.log("After += 5", p); 
+
+p -= 3; 
+console.log("After -= 3", p); 
+
+p *= 2; 
+console.log("After *= 2", p); 
+
+p /= 4;
+console.log("After /= 4", p); 
+
+p %= 5; 
+console.log("After %= 5", p);
+
