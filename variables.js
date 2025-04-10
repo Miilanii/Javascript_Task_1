7b855c3e117fdaa5065cc6da1503e20da63d7eda
Miilanii <milanisobopha5@gmail.com>
2025-04-10 20:20:08 +0200

    Finished all exercises in Variables.js

---
+++ variables.js
@@ -0,0 +1,33 @@
+
+// Exercise 1: Variable Declaration
+
+let name = "Milani"
+let age = 24
+let isStudent = true
+
+if (isStudent){
+    console.log("My name is", name,",I am ", age, " years old and I am a student");
+    
+}else{
+    console.log("Oopss!")
+}
+
+// Exercise 2: Variable
+
+let favoriteColour = "Black"
+console.log("I used to really like the colour",favoriteColour)
+
+favoriteColour = "Navy Blue"
+console.log("But now my favorite color is", favoriteColour)
+
+// Exercise 3: Arithmetic
+
+let num1 = 2001
+let num2 = 2000
+
+let sum = num1 + num2
+let product = num1 * num2
+let difference = num1 - num2
+let quotient = num1 / num2
+
+console.log("The sum, product, difference and quotient of num1 and num2 respectively is:", sum, product, difference, quotient)
